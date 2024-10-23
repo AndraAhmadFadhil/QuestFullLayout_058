7ae058f60df8f79fc9700f924f96d7dfd833205e
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-10-23 15:40:01 +0700

    Activity Full Layout

---
+++ app/src/main/java/com/example/projectpamuntukpertemuan5/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.projectpamuntukpertemuan5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectpamuntukpertemuan5.ui.theme.ProjectPAMUntukPertemuan5Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjectPAMUntukPertemuan5Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                ) {
+                        PlayActivity()
+                    }
+
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProjectPAMUntukPertemuan5Theme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/projectpamuntukpertemuan5/PlayActivity.kt
@@ -0,0 +1,77 @@
+package com.example.projectpamuntukpertemuan5
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun PlayActivity(modifier : Modifier = Modifier){
+    Column(modifier = modifier.fillMaxSize()){
+        SectionHeader()
+        Text(text = "Kepada Yth :")
+        Text(text = "Jodi")
+        Spacer(modifier = Modifier.padding(20.dp))
+        MainSection(
+            judulParam = "Nama", isiParam = "Andra"
+        )
+        MainSection(judulParam = "Kelas", isiParam = "Program Studi Teknologi Informasi")
+        MainSection(judulParam = "NIM", isiParam = "20220140058")
+        MainSection(judulParam = "Keterangan", isiParam = "Anak Sholeh")
+    }
+}
+
+@Composable
+fun SectionHeader() {
+    Box(modifier = Modifier.fillMaxWidth().background(color = Color.DarkGray)) {
+        Row(modifier = Modifier.padding(15.dp)) {
+            Box(contentAlignment = Alignment.BottomEnd){
+                Image(painter = painterResource(id = R.drawable.logo), contentDescription = "", Modifier.size(100.dp).clip(shape = CircleShape))
+                Icon(Icons.Filled.Check, contentDescription = "", Modifier.padding(end = 15.dp).size(25.dp), tint = Color.Red)
+            }
+
+            Column(Modifier.padding(15.dp)) {
+                Text(text = "Teknologi Informasi", color = Color.White)
+                Spacer(Modifier.padding(3.dp))
+                Text(text = "Universitas Gamping Mengidul", color = Color.White)
+            }
+
+        }
+
+
+    }
+}
+
+@Composable
+fun MainSection(judulParam:String, isiParam:String) {
+    Column(horizontalAlignment = Alignment.Start, modifier = Modifier.padding(8.dp)) {
+
+        Row(modifier = Modifier.fillMaxWidth().padding(8.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(text = judulParam, modifier = Modifier.weight(0.8f))
+            Text(text = ":", modifier = Modifier.weight(0.2f))
+            Text(text = isiParam, modifier = Modifier.weight(2f))
+        }
+    }
+}
